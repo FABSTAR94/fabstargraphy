9541a430f7b15f07f63652edf2d25d74ba9df20c
Faviola Caballero <fabycg18@gmail.com>
2020-04-09 21:54:22 -0700

    added folder and files and styled about section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,96 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
 import './App.css';
+import About from '../About/About.jsx';
+import Home from '../Home/Home.jsx';
+import Services from '../Services/Services.jsx';
+import Gallery from '../Gallery/Gallery.jsx';
+import Contact from '../Contact/Contact.jsx';
+
+const SIDEBAR_OPTIONS = [
+  'HOME',
+  'ABOUT',
+  'SERVICES',
+  'GALLERY',
+  'CONTACT',
+];
 
 class App extends React.Component {
+  state = {
+    currentComponent: 'HOME',
+  }
 
-render(){
-  return(
-    <div>
-    <section id="sidebar">
-    <header>
-    <img class="logo" src={logo}/>
-    <a><h1>Faviola Caballero<br/><span>Fabstargraphy</span></h1></a>
-    </header>
-    <ul class="side-menu">
-    <li><a href='#'>Home</a></li>
-    <li><a href='#'>About</a></li>
-     <li><a href='#'>Services</a></li>
-    <li><a href='#'>Gallery</a></li>
-    <li><a href='#'>Contact</a></li>
-    </ul>
+  handleSideBarOptionClick(idx) {
+    this.setState({ currentComponent: SIDEBAR_OPTIONS[idx] })
+  }
+
+  renderViewComponent() {
+    const { currentComponent } = this.state;
+
+    switch(currentComponent) {
+      case 'HOME':
+        return (
+          <Home />
+        );
+      case 'ABOUT':
+        return (
+          <About />
+        );
+      case 'SERVICES':
+        return (
+          <Services />
+        );
+      case 'GALLERY':
+        return(
+          <Gallery />
+        );
+      case 'CONTACT':
+        return(
+          <Contact />
+        );
+      default:
+        return null;
+    }
+  }
+
+  render(){
+    return(
+      <div>
+      <section id="sidebar">
+      <header>
+      <img class="logo" src={logo}/>
+      <a><h1>Faviola Caballero<br/><span>Fabstargraphy</span></h1></a>
+      </header>
+      <ul class="side-menu">
+        <li><a onClick={() => this.handleSideBarOptionClick(0)} href="#Home">Home</a></li>
+        <li><a onClick={() => this.handleSideBarOptionClick(1)} href="#About">About</a></li>
+        <li><a onClick={() => this.handleSideBarOptionClick(2)} href="#Services">Services</a></li>
+        <li><a onClick={() => this.handleSideBarOptionClick(3)} href="#Gallery">Gallery</a></li>
+        <li><a onClick={() => this.handleSideBarOptionClick(4)} href="#Contact">Contact</a></li>
+      </ul>
 
 
-    <footer>
-    <ul>
-    <li><a href="https://www.instagram.com/fabstar_94/" target="blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-    <li><a href="https://www.linkedin.com/in/fabstar94-faviola-18fcg79341/" target="blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-    <li><a href="https://www.linkedin.com/in/fabstar94-faviola-18fcg79341/" target="blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-    <li><a href="https://www.linkedin.com/in/fabstar94-faviola-18fcg79341/" target="blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
-    </ul>
-    <small>ⓒ2020 Fabstargraphy</small>
-    </footer>
-    </section>
-</div>
+      <footer>
+      <ul>
+      <li><a href="https://www.instagram.com/fabstar_94/" target="blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/fabstar94-faviola-18fcg79341/" target="blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/fabstar94-faviola-18fcg79341/" target="blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/fabstar94-faviola-18fcg79341/" target="blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+      </ul>
+      <small>ⓒ2020 Fabstargraphy</small>
+      </footer>
+      </section>
 
-
+      <div class="sections">
+        {this.renderViewComponent()}
+      </div>
+      </div>
   )
 }
 }
+
+
+
 // function App() {
 //   return (
 //     <div className="App">
